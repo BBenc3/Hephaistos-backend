519fab09d72be7cf0eb89897207f27aa0e929034
BBenc3 <biliczkib@kkszki.hu>
2025-02-10 08:28:39 +0100

    Add Course, Teacher, Userdata, Student, and Lesson models with CreatedAt property; update AuthController and HephaistosContext

--- Migrations/20250204093313_RecreateDatabase.Designer.cs
+++ Migrations/20250204093313_RecreateDatabase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectHephaistos.Data;
 
@@ -11,9 +12,11 @@ using ProjectHephaistos.Data;
 namespace ProjectHephaistos.Migrations
 {
     [DbContext(typeof(HephaistosContext))]
-    partial class HephaistosContextModelSnapshot : ModelSnapshot
+    [Migration("20250204093313_RecreateDatabase")]
+    partial class RecreateDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -72,7 +75,10 @@ namespace ProjectHephaistos.Migrations
                     b.Property<DateTime>("EndDate")
                         .HasColumnType("datetime(6)");
 
-                    b.Property<int?>("PreviousLessonId")
+                    b.Property<int?>("LessonId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("PreviousLessonId")
                         .HasColumnType("int");
 
                     b.Property<DateTime>("StartDate")
@@ -277,7 +283,9 @@ namespace ProjectHephaistos.Migrations
                 {
                     b.HasOne("ProjectHephaistos.Models.Lesson", "PreviousLesson")
                         .WithMany()
-                        .HasForeignKey("PreviousLessonId");
+                        .HasForeignKey("PreviousLessonId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
 
                     b.HasOne("ProjectHephaistos.Models.Teacher", "Teacher")
                         .WithOne("TeacherOfLesson")
--- obj/Debug/net8.0/ProjectHephaistos.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectHephaistos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectHephaistos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9900b9d07886a2cc97ab89d998740408094a5517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84367697ce99579944cefbad529924f93840a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectHephaistos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectHephaistos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
